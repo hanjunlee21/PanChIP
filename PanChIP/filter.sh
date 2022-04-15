8c8c4553f6d8a47831ee7f2362d34618d093a2dd
hanjunlee21 <67846757+hanjunlee21@users.noreply.github.com>
2022-04-15 06:46:18 -0400

    Update filter.sh

--- PanChIP/filter.sh
+++ PanChIP/filter.sh
@@ -36,7 +36,7 @@ do
 done
 printf ""
 wait
-seq $Filter | sed 's:.*:'$sedlib'\/'$rep'\/&.sum:' | xargs cat > $lib/$rep/SUM.count
+seq $Filter | sed 's:.*:'$sedlib'\/'$rep'\/&.sum:' | xargs cat > $lib/SUM.count
 seq $Filter | sed 's:.*:'$sedlib'\/'$rep'\/&.sum:' | xargs rm
 
 subtask1() {
@@ -53,7 +53,7 @@ rm $output/$2/$1/intersect.*.count
 rm $output/$2/$1/intersect2.*.count
 sort -u -k1,1 -k2,2n -k3,3n -k4,4n $input/$1.bed | awk 'function abs(v) {return v < 0 ? -v : v} BEGIN{var=0} {var=var+$5*abs($3-$2)} END{print var}' > $output/$2/$1/$1.dist
 awk '{for(i=1;i<='$Filter';i++) {print}}' $output/$2/$1/$1.dist > $output/$2/$1/$1.tmp
-paste $output/$2/$1/intersect.dist $output/$2/$1/intersect2.dist $lib/$2/SUM.count $output/$2/$1/$1.tmp | awk '{print sqrt($1*$2/$3/$4)}' > $output/$2/$1/intersect.normalized.dist
+paste $output/$2/$1/intersect.dist $output/$2/$1/intersect2.dist $lib/SUM.count $output/$2/$1/$1.tmp | awk '{print sqrt($1*$2/$3/$4)}' > $output/$2/$1/intersect.normalized.dist
 rm $output/$2/$1/$1.tmp $output/$2/$1/intersect.dist $output/$2/$1/intersect2.dist
 }
 task1() {
